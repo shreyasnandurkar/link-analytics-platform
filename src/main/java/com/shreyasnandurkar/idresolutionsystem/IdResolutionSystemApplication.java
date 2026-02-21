3134dd3d85fe3bdab5e7711a7801406ebeff72bd
Lithium-superoxide <nandurkar.mitblr2023@learner.manipal.edu>
2026-02-21 19:19:51 +0530

    Skeleton commit

---
+++ src/main/java/com/shreyasnandurkar/idresolutionsystem/IdResolutionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shreyasnandurkar.idresolutionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdResolutionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdResolutionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shreyasnandurkar/idresolutionsystem/IdResolutionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyasnandurkar.idresolutionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdResolutionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
