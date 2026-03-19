a1c7ef7ca8963988600ecb084b89f13fc82c1b51
Lithium-superoxide <nandurkar.mitblr2023@learner.manipal.edu>
2026-03-19 17:58:56 +0530

    Optimize short key generation by initializing StringBuilder with capacity and placing CHARS string outside the loop.

--- src/main/java/com/shreyasnandurkar/idresolutionsystem/service/ShortKeyGenerator.java
+++ src/main/java/com/shreyasnandurkar/idresolutionsystem/service/ShortKeyGenerator.java
@@ -7,9 +7,9 @@ public class ShortKeyGenerator {
     private static final SecureRandom random = new SecureRandom();
 
     public static String generateShortKey(){
-        StringBuilder shortKey = new StringBuilder();
+        StringBuilder shortKey = new StringBuilder(6);
+        String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         for (int i = 0; i < 6; i++) {
-            String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
             shortKey.append(CHARS.charAt(random.nextInt(CHARS.length())));
         }
         return shortKey.toString();
