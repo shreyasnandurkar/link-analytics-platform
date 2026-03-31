87cddf9e82a86f0fb6461bec76fd4b51ee1f73da
Lithium-superoxide <nandurkar.mitblr2023@learner.manipal.edu>
2026-03-31 14:55:34 +0530

    Fast lookup whether key exists in DB or not

---
+++ src/main/java/com/shreyasnandurkar/idresolutionsystem/config/KeyStore.java
@@ -0,0 +1,35 @@
+package com.shreyasnandurkar.idresolutionsystem.config;
+
+import com.shreyasnandurkar.idresolutionsystem.repository.WebsiteUrlRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Component
+public class KeyStore {
+    private final Set<String> keySet = ConcurrentHashMap.newKeySet();
+    private final WebsiteUrlRepository urlRepository;
+
+    public KeyStore(WebsiteUrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
+    @PostConstruct
+    public void loadKeys() {
+        List<String> keys = urlRepository.findAllshortKey();
+        keySet.addAll(keys);
+    }
+
+    public void addKey(String key){
+        keySet.add(key);
+    }
+
+    public boolean contains(String key){
+        return keySet.contains(key);
+    }
+}
