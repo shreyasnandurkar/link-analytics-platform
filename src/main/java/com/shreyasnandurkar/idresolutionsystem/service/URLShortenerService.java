1f47ba576502fb0248f511e1b8db516b5ec301b9
Lithium-superoxide <nandurkar.mitblr2023@learner.manipal.edu>
2026-03-31 14:56:47 +0530

    Improve short link creation by using KeyStore for key uniqueness and removing unnecessary transaction management

--- src/main/java/com/shreyasnandurkar/idresolutionsystem/service/URLShortenerService.java
+++ src/main/java/com/shreyasnandurkar/idresolutionsystem/service/URLShortenerService.java
@@ -1,13 +1,12 @@
     package com.shreyasnandurkar.idresolutionsystem.service;
 
     import com.shreyasnandurkar.idresolutionsystem.config.AppProperties;
+    import com.shreyasnandurkar.idresolutionsystem.config.KeyStore;
     import com.shreyasnandurkar.idresolutionsystem.entity.LinkType;
     import com.shreyasnandurkar.idresolutionsystem.entity.WebsiteUrl;
     import com.shreyasnandurkar.idresolutionsystem.repository.WebsiteUrlRepository;
     import org.springframework.dao.DuplicateKeyException;
     import org.springframework.stereotype.Service;
-    import org.springframework.transaction.annotation.Transactional;
-
 
     @Service
     public class URLShortenerService {
@@ -15,27 +14,34 @@
         private final WebsiteUrlRepository repository;
         private final CounterService counterService;
         private final UrlLookupService urlLookupService;
+        private final KeyStore keyStore;
 
         public URLShortenerService(AppProperties appProperties, WebsiteUrlRepository repository,
-                                   CounterService counterService, UrlLookupService urlLookupService) {
+                                   CounterService counterService, UrlLookupService urlLookupService, KeyStore keyStore) {
             this.appProperties = appProperties;
             this.repository = repository;
             this.counterService = counterService;
             this.urlLookupService = urlLookupService;
+            this.keyStore = keyStore;
         }
 
-        @Transactional
         public String createShortLink(String originalUrl, LinkType type){
-            while (true) {
-                String key = ShortKeyGenerator.generateShortKey();
-                try {
-                    repository.save(new WebsiteUrl(originalUrl, key, type));
-                    return appProperties.getBaseUrl() + "/" + key;
-                } catch (DuplicateKeyException ignored) {}
-            }
+            String shortKey;
+            do {
+                shortKey = ShortKeyGenerator.generateShortKey();
+            } while (keyStore.contains(shortKey));
+
+            try {
+                repository.save(new WebsiteUrl(originalUrl, shortKey, type));
+                keyStore.addKey(shortKey);
+            } catch (DuplicateKeyException ignored) {}
+
+            return appProperties.getBaseUrl() + "/" + shortKey;
         }
 
         public String redirectUrl(String shortKey){
+            if(!keyStore.contains(shortKey)) throw new RuntimeException("Invalid URL");
+
             String originalUrl = urlLookupService.getOriginalUrl(shortKey);
             counterService.incrementRedirectCount(shortKey);
             return originalUrl;
