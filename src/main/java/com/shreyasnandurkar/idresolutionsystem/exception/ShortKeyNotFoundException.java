cc1da4e93edaf09bedc0b024305f9724b09e5abf
Lithium-superoxide <nandurkar.mitblr2023@learner.manipal.edu>
2026-04-12 19:11:30 +0530

    Add ShortKeyNotFoundException to handle missing short key errors

---
+++ src/main/java/com/shreyasnandurkar/idresolutionsystem/exception/ShortKeyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.shreyasnandurkar.idresolutionsystem.exception;
+
+public class ShortKeyNotFoundException extends RuntimeException{
+    public ShortKeyNotFoundException(String message) {
+        super(message);
+    }
+}
