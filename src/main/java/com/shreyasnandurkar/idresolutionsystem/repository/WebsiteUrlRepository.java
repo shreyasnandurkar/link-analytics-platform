f81068d7b56cb533af85a3b5f7aff85aa540f31a
Lithium-superoxide <nandurkar.mitblr2023@learner.manipal.edu>
2026-04-14 12:00:57 +0530

    Remove incrementRedirectCount method from WebsiteUrlRepository

--- src/main/java/com/shreyasnandurkar/idresolutionsystem/repository/WebsiteUrlRepository.java
+++ src/main/java/com/shreyasnandurkar/idresolutionsystem/repository/WebsiteUrlRepository.java
@@ -2,18 +2,14 @@ package com.shreyasnandurkar.idresolutionsystem.repository;
 
 import com.shreyasnandurkar.idresolutionsystem.entity.WebsiteUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface WebsiteUrlRepository extends JpaRepository<WebsiteUrl, UUID> {
-    WebsiteUrl findByShortKey(String shortKey);
 
-    @Modifying
-    @Query("update WebsiteUrl w set w.redirectCount = w.redirectCount+1 where w.shortKey = :shortKey")
-    void incrementRedirectCount(String shortKey);
+    WebsiteUrl findByShortKey(String shortKey);
 
     @Query("select w.shortKey from WebsiteUrl w")
     List<String> findAllshortKey();
